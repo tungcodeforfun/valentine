06accabf7c65e02453acf978b44f77d46ea6fc22
Tung Nguyen <tungnguyen1651@gmail.com>
2026-01-30 01:34:51 -0500

    Add sad rain effect on no screen

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const noScreen = document.getElementById('no-screen');
 
 let rainContainer = null;
 
-function createRain() {
+function createRain(imgSrc) {
     if (rainContainer) rainContainer.remove();
     rainContainer = document.createElement('div');
     rainContainer.className = 'rain-container';
@@ -14,14 +14,14 @@ function createRain() {
 
     for (let i = 0; i < 30; i++) {
         setTimeout(() => {
-            const cat = document.createElement('img');
-            cat.src = 'cat.gif';
-            cat.className = 'falling-cat';
-            cat.style.left = Math.random() * 100 + 'vw';
-            cat.style.width = (40 + Math.random() * 40) + 'px';
-            cat.style.animationDuration = (3 + Math.random() * 3) + 's';
-            rainContainer.appendChild(cat);
-            setTimeout(() => cat.remove(), 6000);
+            const img = document.createElement('img');
+            img.src = imgSrc;
+            img.className = 'falling-cat';
+            img.style.left = Math.random() * 100 + 'vw';
+            img.style.width = (40 + Math.random() * 40) + 'px';
+            img.style.animationDuration = (3 + Math.random() * 3) + 's';
+            rainContainer.appendChild(img);
+            setTimeout(() => img.remove(), 6000);
         }, i * 200);
     }
 }
@@ -29,12 +29,13 @@ function createRain() {
 yesBtn.addEventListener('click', function() {
     questionScreen.classList.add('hidden');
     successScreen.classList.remove('hidden');
-    createRain();
+    createRain('cat.gif');
 });
 
 noBtn.addEventListener('click', function() {
     questionScreen.classList.add('hidden');
     noScreen.classList.remove('hidden');
+    createRain('rain-sad.gif');
 });
 
 document.getElementById('back-yes').addEventListener('click', function() {
@@ -46,4 +47,5 @@ document.getElementById('back-yes').addEventListener('click', function() {
 document.getElementById('back-no').addEventListener('click', function() {
     noScreen.classList.add('hidden');
     questionScreen.classList.remove('hidden');
+    if (rainContainer) rainContainer.remove();
 });
